b5024e5373e715c73e2c0bb3af7e8e9d00a226ce
Vinit <kumarvinit86@gmail.com>
2021-10-22 16:53:08 +0530

    for hiding user id from documentation

--- src/Adform.Todo.Dto/Item.cs
+++ src/Adform.Todo.Dto/Item.cs
@@ -1,10 +1,13 @@
-﻿namespace Adform.Todo.Dto
+﻿using System.Text.Json.Serialization;
+
+namespace Adform.Todo.Dto
 {
     public class Item 
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string LabelName { get; set; }
+        [JsonIgnore]
         public int UserId { get; set; }
     }
 }
